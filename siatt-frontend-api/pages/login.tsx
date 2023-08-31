a3555e58438788a9433ed760903d29e37a64a578
otavioabreu27 <otavio.abreu96@gmail.com>
2023-08-31 17:49:26 -0300

    adding the pages to next

---
+++ siatt-frontend-api/pages/login.tsx
@@ -0,0 +1,25 @@
+import Head from 'next/head';
+import React from 'react';
+import '../public/css/login.css';
+
+export default function Login() {
+  return(
+    <>
+      <Head>
+        <title>Login</title>
+      </Head>
+        <h1 id="logo">M1</h1>
+        <div className="form-block">
+          <div className="form-field">
+            <h2 className="form-label">Usuário</h2>
+            <input type="text" name="usuario" className="form-input"></input>
+          </div>
+          <div className="form-field">
+            <h2 className="form-label">Senha</h2>
+            <input type="password" name="usuario" className="form-input"></input>
+          </div>
+        <input type="button" value="Login" className="form-submit"></input>
+      </div>
+    </>
+  )
+}
--- siatt-frontend-api/pages/projetos.tsx
+++ siatt-frontend-api/pages/projetos.tsx
@@ -1,14 +1,14 @@
-<!DOCTYPE html>
-<html lang="pt-br">
-<head>
-  <meta charset="UTF-8">
-  <meta name="viewport" content="width=device-width, initial-scale=1.0">
-  <link rel="stylesheet" href="../public/css/projetos.css">
-  <title>Projetos</title>
-</head>
-<body>
-  <div id="__next">
-    <nav>
+import Head from 'next/head';
+import React from 'react';
+import '../public/css/projetos.css';
+
+export default function Login() {
+  return(
+    <>
+      <Head>
+        <title>Projetos</title>
+      </Head>
+      <nav>
       <h3>M1</h3>
       <ul>
         <li><a href="#">Projetos</a></li>
@@ -16,40 +16,40 @@
         <li><a href="#">Painel</a></li>
       </ul>
     </nav>
-    <div class="conteudo">
-      <input type="button" value="+ Adicionar Projeto" id="botao-projetos">
+    <div className="conteudo">
+      <input type="button" value="+ Adicionar Projeto" id="botao-projetos"></input>
       <div id="projetos">
-        <div class="card-projeto">
+        <div className="card-projeto">
           <span>Sistema de Reconhecimento de motociclistas</span>
           <span>Entrega: 27/10/2002</span>
           <span>Progresso: 0%</span>
         </div>
-        <div class="card-projeto">
+        <div className="card-projeto">
           <span>Sistema de Reconhecimento de motociclistas</span>
           <span>Entrega: 27/10/2002</span>
           <span>Progresso: 0%</span>
         </div>
-        <div class="card-projeto">
+        <div className="card-projeto">
           <span>Sistema de Reconhecimento de motociclistas</span>
           <span>Entrega: 27/10/2002</span>
           <span>Progresso: 0%</span>
         </div>
-        <div class="card-projeto">
+        <div className="card-projeto">
           <span>Sistema de Reconhecimento de motociclistas</span>
           <span>Entrega: 27/10/2002</span>
           <span>Progresso: 0%</span>
         </div>
-        <div class="card-projeto">
+        <div className="card-projeto">
           <span>Sistema de Reconhecimento de motociclistas</span>
           <span>Entrega: 27/10/2002</span>
           <span>Progresso: 0%</span>
         </div>
-        <div class="card-projeto">
+        <div className="card-projeto">
           <span>Sistema de Reconhecimento de motociclistas</span>
           <span>Entrega: 27/10/2002</span>
           <span>Progresso: 0%</span>
         </div>
-        <div class="card-projeto">
+        <div className="card-projeto">
           <span>Sistema de Reconhecimento de motociclistas</span>
           <span>Entrega: 27/10/2002</span>
           <span>Progresso: 0%</span>
@@ -58,7 +58,7 @@
     </div>
     <footer>
       <span>Grupo M1© • 2023 • Todos direitos reservados</span>
-    </footer>
-  </div>
-</body>
-</html>
+    </footer>    
+    </>
+  )
+}
