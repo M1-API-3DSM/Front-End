63ec2e39f8a378d315a468aaffc09c96dff57fb7
Felipe Augusto Graciano <felipe98ju@hotmail.com>
2023-09-12 15:19:12 -0300

    Criação da tela de novo projeto

---
+++ siatt-frontend-api/pages/novoprojeto/index.tsx
@@ -0,0 +1,39 @@
+import Head from 'next/head';
+import React from 'react';
+import Nav, {Papel, NavProps} from '../../components/nav';
+import Footer from '../../components/footer';
+import UploadaerXlsx from '../../components/uploader_xlsx';
+import '../../public/css/novoprojeto.css';
+
+
+export async function getServerSideProps() {
+  const nav: NavProps = {papel: Papel.EngenheiroChefe}
+  return {
+    props: {
+      nav
+    },
+  };
+}
+
+function Projetos({ nav} : { nav: NavProps}) {
+  return(
+    <>
+      <Head>
+        <title>Projetos</title>
+      </Head>
+      <Nav {...nav} />
+      <div className="conteudo">
+        <div className="btnNovoProjeto">
+            <label>Nome do projeto: </label><input type='text' ></input>
+        </div>
+        <div id="projetos">
+            <UploadaerXlsx rota="/Rota_Tarefas" />
+            <UploadaerXlsx rota="/Rota_Recursos" />
+        </div>
+      </div>
+      <Footer />
+    </>
+  )
+}
+
+export default Projetos;
