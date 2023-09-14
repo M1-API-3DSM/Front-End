371fa296c30a2478178cf438a713c9dce38abe3a
otavioabreu27 <otavio.abreu96@gmail.com>
2023-09-14 10:03:46 -0300

    alterações de css login

--- siatt-frontend-api/pages/projetos/[id].tsx
+++ siatt-frontend-api/pages/projetos/[id].tsx
@@ -1,17 +1,29 @@
 import { useRouter } from 'next/router'
 import { GetServerSideProps } from 'next';
  
-export default function Projeto({ id }: { id: number }) {
+export default function Projeto({ id, data }: { id: number, data: String[] }) {
   const router = useRouter()
-  return <p>Projeto: {id}</p>
+  return(
+    <>
+    {data.map((projeto, index) =>{
+      return(
+        <a key={index} href={`projetos/${id}/${projeto}`}>
+          {projeto}
+        </a>
+      )
+    })}
+    </>
+  )
 }
 
 export async function getServerSideProps(context: any) {
   const { id } = context.query;
+  const data = ['1.1', '1.1.1', '1.1.2', '1.1.3']
 
   return {
     props: {
       id,
+      data
     },
   };
 }
